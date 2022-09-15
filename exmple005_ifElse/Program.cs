84325feff2dca711a11a3cb5aa0d7afd8ef12d85
Aleksandr <alexchembarov@gmail.com>
2022-09-15 18:56:54 +0400

    exmple005_ifElse add

---
+++ exmple005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter Name:");
+string username = Console.ReadLine();
+if(username.ToLower() == "nadya")
+{
+    Console.WriteLine("Uraaa eto Nad`kaaa");
+}
+else
+{
+    Console.Write("Hi,");
+    Console.WriteLine(username);
+}
